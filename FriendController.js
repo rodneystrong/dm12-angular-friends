0506895cead15257ea1b3adad4b524a14a81de56
aleeex <akang2@gmail.com>
2016-08-24 14:07:36 -0600

    added friends to service and got app to work

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,12 @@
 angular
   .module('friendBook')
-  .controller('MainCtrl', function($scope) {
+  .controller('MainCtrl', function($scope, friendService) {
     $scope.thisAppIsBroken = "Good job you suck";
+
+    $scope.getFriends = function() {
+        $scope.friends = friendService.getFriends();
+    }
+
+      $scope.getFriends();
+
   })
